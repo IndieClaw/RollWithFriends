d9772a2d4c03c0e560204cd96e6793395724f52d
ClawIndieGames <clawindieGames@gmail.com>
2019-12-25 22:45:59 +0000

    Basic camera and ball movement. camer abug

---
+++ RollWithFriends/Assets/Scripts/Controllers/CameraManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    #region Fields and properties
+    public static CameraManager instance;
+
+    [SerializeField] Vector3 cameraPlayerOffset;
+
+    [SerializeField]
+    public Transform target;
+
+    [SerializeField]
+    float controllerSpeed = 8;
+
+    [SerializeField] private Transform pivot;
+    //[HideInInspector] public Transform cameraTransform;
+
+    float turnSmoothing = .1f;
+
+    [SerializeField]
+    float minAngle = -90;
+
+    [SerializeField]
+    float maxAngle = 90;
+
+    float smoothX;
+    float smoothVelocityX;
+    float smoothY;
+    float smoothVelocityY;
+
+    float lookAngle;
+    float tiltAngle = 20;
+    
+    #endregion
+
+    #region Public methods
+
+    #endregion
+
+
+    #region Private methods	
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    void Start()
+    {
+        pivot.transform.position += cameraPlayerOffset;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            FollowTarget();
+            HandleRotations(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight"));
+        }
+    }
+
+    void FollowTarget()
+    {
+        transform.position = target.transform.position;
+    }
+
+    void HandleRotations(float vertical, float horizontal)
+    {
+        if (turnSmoothing > 0)
+        {
+            smoothX = Mathf.SmoothDamp(smoothX, vertical, ref smoothVelocityX, turnSmoothing);
+            smoothY = Mathf.SmoothDamp(smoothY, horizontal, ref smoothVelocityY, turnSmoothing);
+        }
+        else
+        {
+            smoothX = horizontal;
+            smoothY = vertical;
+        }
+
+        lookAngle += smoothX * controllerSpeed;
+        transform.rotation = Quaternion.Euler(0, lookAngle, 0);
+
+        tiltAngle -= smoothY * controllerSpeed;
+        tiltAngle = Mathf.Clamp(tiltAngle, minAngle, maxAngle);
+        pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
+    }
+
+    
+    #endregion
+}
---
+++ RollWithFriends/Assets/Scripts/Controllers/PlayerMovementController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    #region Fields and properties
+    [SerializeField] private float speed = 8;
+    [SerializeField] private int jumpForce = 300;
+
+    [SerializeField] private Rigidbody childRb;
+
+    private bool canJump;
+
+    #endregion
+
+    #region Public methods
+
+    #endregion
+
+
+    #region Private methods	
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));            
+        }
+    }
+
+    // void FixedUpdate()
+    // {
+    //     if (!hasAuthority)
+    //     {
+    //         return;
+    //     }        
+
+    //     if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+    //     {
+    //         MovePlayer(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));            
+    //     }
+        
+    // }
+
+    void Jump()
+    {
+        if (canJump)
+        {
+            childRb.AddForce(new Vector3(0, jumpForce, 0));
+            canJump = false;
+        }
+    }
+
+    void Move(float horizontalInput, float verticalInput)
+    {
+        // camera orientation
+        Vector3 verticalOrientation = Input.GetAxis("Vertical") * CameraManager.instance.transform.forward;
+        Vector3 horizontalOrientation = Input.GetAxis("Horizontal") * CameraManager.instance.transform.right;
+
+        var desiredMoveDirection = (verticalOrientation + horizontalOrientation).normalized;
+
+        childRb.AddForce(desiredMoveDirection * speed);
+    }
+
+
+    private IEnumerator AssignCameraTarget()
+    {
+        yield return new WaitForSeconds(.01f);
+
+        CameraManager.instance.target = gameObject.transform;
+    }
+    #endregion
+
+    #region Colision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+    #endregion
+}
