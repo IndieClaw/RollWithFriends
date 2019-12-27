e126ef424417285c200b7c031981e7ee31231fa1
ClawIndieGames <clawindieGames@gmail.com>
2019-12-27 17:50:25 +0000

    added checkpoints

---
+++ RollWithFriends/Assets/Scripts/Components/Checkpoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    #region Fields and properties
+    public enum CheckpointType
+    {
+        Start,
+        End,
+        Checkpoint
+    }
+
+[SerializeField] private CheckpointType checkpointType;
+[SerializeField] private Checkpoint previousCheckpoint;
+
+[SerializeField] private Checkpoint nextCheckpoint;
+
+#endregion
+
+#region Public methods
+
+#endregion
+
+
+#region Private methods	
+
+void Start()
+{
+
+}
+
+void Update()
+{
+
+}
+	
+	#endregion
+}
