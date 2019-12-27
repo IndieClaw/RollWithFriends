12b1e84d1955cfae01f826653bd92f2c4fdb2350
ClawIndieGames <clawindieGames@gmail.com>
2019-12-27 21:46:11 +0000

    Transparency Stipple camera effect

--- RollWithFriends/Assets/Scripts/Controllers/CameraManager.cs
+++ RollWithFriends/Assets/Scripts/Controllers/CameraManager.cs
@@ -33,7 +33,12 @@ public class CameraManager : MonoBehaviour
 
     float lookAngle;
     float tiltAngle = 20;
-    
+
+    Transform obstruction;
+    Material obstructionMaterial;
+    MeshRenderer meshRenderer;
+    [SerializeField] Material transparencyMaterial;
+
     #endregion
 
     #region Public methods
@@ -64,7 +69,6 @@ public class CameraManager : MonoBehaviour
 
     void Update()
     {
-
     }
 
     void LateUpdate()
@@ -74,6 +78,7 @@ public class CameraManager : MonoBehaviour
             FollowTarget();
             HandleRotations(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight"));
             HandleRotations(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            HandleMeshTransparency();
         }
     }
 
@@ -84,9 +89,6 @@ public class CameraManager : MonoBehaviour
 
     void HandleRotations(float vertical, float horizontal)
     {
-        if (vertical == 0 && horizontal == 0) 
-            return;
-
         if (turnSmoothing > 0)
         {
             smoothX = Mathf.SmoothDamp(smoothX, vertical, ref smoothVelocityX, turnSmoothing);
@@ -106,6 +108,40 @@ public class CameraManager : MonoBehaviour
         pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
     }
 
-    
+    void HandleMeshTransparency()
+    {
+        RaycastHit hit;
+
+        var dir = target.position - pivot.transform.position;
+        dir.Normalize();
+
+        if (Physics.Raycast(pivot.transform.position, dir * 10, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject != null)
+            {
+                // Save the material for the object, if the raycast is collidig with the same object we dont want to get the mat.                
+                if (obstruction != hit.transform)
+                {
+                    meshRenderer = hit.transform.gameObject.GetComponent<MeshRenderer>();
+                    obstructionMaterial = meshRenderer.material;
+                }
+
+                obstruction = hit.transform;
+
+                meshRenderer.material = transparencyMaterial;
+                meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            }
+        }
+        else
+        {
+            if (obstruction != null)
+            {
+                meshRenderer.material = obstructionMaterial;
+                meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+            }
+        }
+    }
+
+
     #endregion
 }
