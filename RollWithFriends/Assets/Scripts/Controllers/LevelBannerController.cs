2d22ddfb4e17e9bc7ead7a3ca95c2933627e7e27
ClawIndieGames <clawindieGames@gmail.com>
2020-01-04 15:28:44 +0000

    Added level banner and controller WIP

---
+++ RollWithFriends/Assets/Scripts/Controllers/LevelBannerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelBannerController : MonoBehaviour
+{
+    #region Fields and properties
+    [SerializeField] string levelName;
+    [SerializeField] string sceneToLoad;
+
+    [SerializeField] Image levelImage;
+
+    [SerializeField] TextMeshProUGUI personalBestTimeTextMesh;
+
+    [SerializeField] TextMeshProUGUI firstRankTimeTextMesh;
+
+    #endregion
+
+    #region Public methods
+
+    #endregion
+
+
+    #region Private methods	
+    void SetPersonalBest()
+    {
+        personalBestTimeTextMesh.text = PlayerPrefs.GetFloat("level_" + levelName).ToString();
+    }
+
+    void SetFirstRankTime()
+    {
+        // TODO JS: call HighscoreService here.
+        firstRankTimeTextMesh.text = "0";
+
+    }
+
+    void Start()
+    {
+        SetPersonalBest();
+        SetFirstRankTime();
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
